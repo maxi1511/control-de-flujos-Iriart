49e0f088d4aa50b6d1c6e152cc7362732f472707
Maxi Iriart <maxi.iriart@gmail.com>
2021-10-01 15:33:47 -0300

    primer repositorio

---
+++ desafio/main.js
@@ -0,0 +1,8 @@
+let numero1 = parseInt(prompt("Ingrese un numero entero"))
+let numero2 = parseInt(prompt("Ingrese el otro numero entero"))
+
+if ((numero1 >= 100) && (numero2<= 500)){
+    alert("Los numeros ingresados estan entre 100 y 500 y son: "+numero1+" y "+numero2)
+}else{
+    alert("Los numeros ingresados no estan entre 100 y 500 y son: " +numero1+ " y "+numero2)
+}
